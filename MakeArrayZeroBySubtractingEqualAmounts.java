90723cdd97db52409d12038dcfa77af6dc67f181
Aryan Sharma <157452589+AryanSh33@users.noreply.github.com>
2024-12-26 02:10:54 +0530

    Create MakeArrayZeroBySubtractingEqualAmounts.java

---
+++ MakeArrayZeroBySubtractingEqualAmounts.java
@@ -0,0 +1,11 @@
+class MakeArrayZeroBySubtractingEqualAmounts{
+    public int minimumOperations(int[] nums) {
+        Set<Integer>s=new HashSet<>();
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]>0){
+                s.add(nums[i]);
+            }
+        }
+        return s.size();
+    }
+}
