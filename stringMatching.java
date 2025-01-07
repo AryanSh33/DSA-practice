e78e8574372e8dd9207944975f0d5a6ba876bc34
Aryan Sharma <157452589+AryanSh33@users.noreply.github.com>
2025-01-07 14:13:22 +0530

    Create stringMatching.java

---
+++ stringMatching.java
@@ -0,0 +1,24 @@
+// LEET 1408. String Matching in an Array
+
+//nor optimal solution
+class stringMatching {
+    public List<String> stringMatching(String[] words) {
+        HashSet<String>set=new HashSet<>();
+        for(int i=0;i<words.length;i++){
+            String s=words[i];
+            for(int j=0;j<words.length;j++){
+                if(j==i){
+                   continue;
+                }else{
+String s1=words[j];
+if(s.contains(s1)){
+    set.add(s1);
+}
+}
+            }
+        }   
+        List<String>li=new ArrayList<>(set);
+        return li;
+
+    }
+}
