2fd47a0dd259b2fa1f8a53b4bbfb3e68254af8d6
Aryan Sharma <157452589+AryanSh33@users.noreply.github.com>
2025-01-20 02:17:44 +0530

    Create preorderDFS.java

---
+++ preorderDFS.java
@@ -0,0 +1,36 @@
+// LEET 589. N-ary Tree Preorder Traversal
+
+
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class preorderDFS{
+      List<Integer>li=new ArrayList<>();
+    public List<Integer> preorder(Node root) {//DFS
+      if(root==null){
+        return li;
+      }
+            li.add(root.val);
+        
+for(Node child:root.children){
+         preorder(child);
+    }
+    return li;
+   
+}}
