3c2e1804731d32e134e7c163c19310e406725201
Aryan Sharma <157452589+AryanSh33@users.noreply.github.com>
2025-01-19 23:33:46 +0530

    Create trap.java

---
+++ trap.java
@@ -0,0 +1,30 @@
+//LEET 42. Trapping Rain Water HARD
+
+class trap {
+    public int trap(int[] height) {
+        if(height.length==0) return 0;
+    int left=0,right=height.length-1,maxLeft=0,maxRight=0;//left ptr and right ptr
+    int total=0;//water int the bucket
+    while(left<right){
+        if(height[left]<height[right]){//if current>max we cant occupy and update max otherwise occupy
+            if(height[left]>maxLeft){
+                maxLeft=height[left];
+            }
+            else{
+                total+=maxLeft-height[left];
+            } 
+            left++;
+        }
+        else{
+              if(height[right]>maxRight){//if current>max we cant occupy and update max otherwise occupy
+                maxRight=height[right];
+            }
+            else{
+                total+=maxRight-height[right];
+            } 
+            right--;
+        }
+    }    
+    return total;
+    }
+}
