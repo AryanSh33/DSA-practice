e5ba797d7b645dfe304d90884713170ce698068d
Aryan Sharma <157452589+AryanSh33@users.noreply.github.com>
2025-02-05 18:50:07 +0530

    Create isSameAfterReversals.java

---
+++ isSameAfterReversals.java
@@ -0,0 +1,8 @@
+class isSameAfterReversals {
+    public boolean isSameAfterReversals(int num) {
+        if(num<10 || num%10!=0){
+            return true;
+        }
+        return false;
+    }
+}
