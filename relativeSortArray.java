7a207721d8cc6dfcabb7029f027363f99a533fed
Aryan Sharma <157452589+AryanSh33@users.noreply.github.com>
2025-02-09 16:03:54 +0530

    Create relativeSortArray.java

---
+++ relativeSortArray.java
@@ -0,0 +1,26 @@
+//LEET.1122. Relative Sort Array
+class relativeSortArray {
+    public int[] relativeSortArray(int[] arr1, int[] arr2) {
+        int z=0,n=arr1.length;
+        for(int i=0;i<arr2.length;i++){
+            for(int j=0;j<n;j++){
+                if(arr1[j]==arr2[i]){
+                    int temp=arr1[z];
+                    arr1[z++]=arr1[j];
+                    arr1[j]=temp;
+                }
+            }
+        }
+       int []arr=new int [n-z];
+       int x=0;
+       for(int i=z;i<n;i++){
+        arr[x++]=arr1[i];
+       }
+       Arrays.sort(arr);
+       x=0;
+       for(int i=z;i<n;i++){
+        arr1[i]=arr[x++];
+       }
+        return arr1;
+    }
+}
