083599a16ec4878372a9126d334e459c9155d1d3
Aryan Sharma <157452589+AryanSh33@users.noreply.github.com>
2025-02-18 00:40:02 +0530

    Create reverseLinkedListII.java

---
+++ reverseLinkedListII.java
@@ -0,0 +1,33 @@
+//92. Reverse Linked List II
+class reverseLinkedListII {
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        ListNode cur=head;
+        int count=0,z=0;
+        while(cur!=null){
+            cur=cur.next;
+            count++;
+        }
+        cur=head;
+        int []arr=new int[count];
+        while(cur!=null){
+            arr[z++]=cur.val;
+            cur=cur.next;
+        }
+    int l=left-1;
+    int r=right-1;
+    while(l<r){
+        int temp=arr[l];
+        arr[l]=arr[r];
+        arr[r]=temp;
+        l++;
+        r--;
+    }
+    z=0;
+    cur=head;
+    while(cur!=null){
+        cur.val=arr[z++];
+        cur=cur.next;
+    } 
+    return head;
+    }
+}
