dff0c48a4c9e2baa99082e703f2d722771ef5e49
Aryan Sharma <157452589+AryanSh33@users.noreply.github.com>
2025-02-26 18:56:45 +0530

    Create removeDuplicates .java

---
+++ removeDuplicates .java
@@ -0,0 +1,16 @@
+//LEET 80. Remove Duplicates from Sorted Array II
+class removeDuplicates {
+    public int removeDuplicates(int[] nums) {
+        if(nums.length<3){
+            return nums.length;
+        }
+        int j=2;
+        for(int i=2;i<nums.length;i++){
+            if(nums[i]!=nums[j-2]){
+                nums[j]=nums[i];
+                j++;
+            }
+        }
+return j;
+    }
+}
