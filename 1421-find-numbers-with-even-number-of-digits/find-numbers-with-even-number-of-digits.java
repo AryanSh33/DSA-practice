3c58974f8661941d113d099f6dd341cccac90b47
Aryan Sharma <157452589+AryanSh33@users.noreply.github.com>
2025-04-30 18:06:55 +0530

    Update find-numbers-with-even-number-of-digits.java

--- 1421-find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.java
+++ 1421-find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.java
@@ -3,7 +3,7 @@ class Solution {
       int count=0;
       for(int v:nums){
         if((v>9&&v<100)||(v>999&&v<10000)||(v==100000)){
-            count++;
+           count++;
         }
       }  
       return count;
