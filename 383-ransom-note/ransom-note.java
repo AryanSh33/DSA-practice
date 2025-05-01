5bc05c8bb349a0467eb445f1be48aa7712ac3490
Aryan Sharma <157452589+AryanSh33@users.noreply.github.com>
2025-05-01 11:43:33 +0530

    Time: 3 ms (70.30%) | Memory: 44.8 MB (70.09%) - LeetSync

---
+++ 383-ransom-note/ransom-note.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        int freq[]=new int[26];
+        for(int i=0;i<magazine.length();i++){
+            char c=magazine.charAt(i);
+            freq[c-'a']++;
+        }
+        for(int i=0;i<ransomNote.length();i++){
+            char c=ransomNote.charAt(i);
+            freq[c-'a']--;
+        }
+        for(int v:freq){
+            if(v<0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
