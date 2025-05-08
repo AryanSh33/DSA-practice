693f675a1c8257b483e46b1969fbf44d6efe5ef2
Aryan Sharma <157452589+AryanSh33@users.noreply.github.com>
2025-05-08 11:39:57 +0530

    Time: 25 ms (17.17%), Space: 56.6 MB (80.07%) - LeetHub

---
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.java
@@ -0,0 +1,17 @@
+class Solution {
+    public List<Integer> findDuplicates(int[] nums) {
+        HashSet<Integer>set1=new HashSet<>();
+        HashSet<Integer>set2=new HashSet<>();
+        for(int v:nums){
+if(set1.contains(v)){
+    set2.add(v);
+}
+else{
+    set1.add(v);
+}
+
+        }
+        List<Integer>li=new ArrayList<>(set2);
+        return li;
+    }
+}
